0e989fb0d3329fd55274f56b276013771394440c
dnspavankumar <dnspavankumr722@gmail.com>
2025-08-28 13:18:53 +0530

    new file:   README.md

--- backend/routes/route.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import handlemessage from "../controllers/messageControllers";
-const router = express.Router();
-
-router.post("/chat",handlemessage);
-
-export default router;
